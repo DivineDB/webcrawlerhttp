6330f09b40f624bdc583108b83ca07cf8840e22f
Divyansh Baghel <divyanshbaghel456@gmail.com>
2025-08-29 00:03:00 +0530

    Crawl single  page with error handling

--- crawl.js
+++ crawl.js
@@ -1,4 +1,29 @@
 const { JSDOM } = require("jsdom");
+const fetch = require("node-fetch");
+
+async function crawlPage(currentURL) {
+	console.log(`Currently Crawling ${currentURL}...`);
+	try {
+		const resp = await fetch(currentURL);
+		if (resp.status > 399) {
+			console.log(
+				`Error in Fetch with Status Code : ${resp.status} On Page${currentURL}`
+			);
+			return;
+		}
+
+		const contentType = resp.headers.get("content-type");
+		if (!contentType.includes("text/html")) {
+			console.log(
+				`Non-Html response, content type: ${contentType} on page : ${currentURL}`
+			);
+			return;
+		}
+		console.log(await resp.text());
+	} catch (err) {
+		console.log(`Error in Fetch : ${err.message} on Page : ${currentURL}`);
+	}
+}
 
 function getURLsFromHTML(htmlBody, baseURL) {
 	const urls = [];
@@ -7,20 +32,18 @@ function getURLsFromHTML(htmlBody, baseURL) {
 	for (const linkElement of linkElements) {
 		if (linkElement.href.slice(0, 1) === "/") {
 			//relative url
-			try{
+			try {
 				const urlObj = new URL(`${baseURL}${linkElement.href}`);
-			urls.push(`${urlObj.href}`);
-		}
-			catch(err){
+				urls.push(`${urlObj.href}`);
+			} catch (err) {
 				console.log(`error with absolute URL : ${err.message}`);
 			}
 		} else {
 			//absolute
-			try{
-				const urlObj = new URL(linkElement.href)
-				urls.push(urlObj.href)
-			}
-			catch(err){
+			try {
+				const urlObj = new URL(linkElement.href);
+				urls.push(urlObj.href);
+			} catch (err) {
 				console.log(`error with relative URL : ${err.message}`);
 			}
 		}
@@ -41,4 +64,5 @@ function normalizeURL(urlString) {
 module.exports = {
 	normalizeURL,
 	getURLsFromHTML,
+	crawlPage,
 };
--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("hi");
+const { crawlPage } = require("./crawl.js");
+
+function main() {
+	if (process.argv.length < 3) {
+		console.log("No Website Provided");
+		process.exit(1);
+	}
+	if (process.argv.length > 3) {
+		console.log("Please Enter Only One Website");
+		process.exit(1);
+	}
+	const baseURL = process.argv[2];
+
+	console.log(`Starting Web Crawl On ${baseURL}`);
+	crawlPage(baseURL);
+}
+
+main();
