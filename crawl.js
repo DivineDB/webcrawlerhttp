f9c22d0fde63cc1655ab1f9c371743cb52034db8
Divyansh Baghel <divyanshbaghel456@gmail.com>
2025-08-28 22:53:04 +0530

    GetURLsfromHtml function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,33 @@
+const { JSDOM } = require("jsdom");
+
+function getURLsFromHTML(htmlBody, baseURL) {
+	const urls = [];
+	const dom = new JSDOM(htmlBody);
+	const linkElements = dom.window.document.querySelectorAll("a");
+	for (const linkElement of linkElements) {
+		if (linkElement.href.slice(0, 1) === "/") {
+			//relative url
+			try{
+				const urlObj = new URL(`${baseURL}${linkElement.href}`);
+			urls.push(`${urlObj.href}`);
+		}
+			catch(err){
+				console.log(`error with absolute URL : ${err.message}`);
+			}
+		} else {
+			//absolute
+			try{
+				const urlObj = new URL(linkElement.href)
+				urls.push(urlObj.href)
+			}
+			catch(err){
+				console.log(`error with relative URL : ${err.message}`);
+			}
+		}
+	}
+	return urls;
+}
+
 function normalizeURL(urlString) {
 	const urlobj = new URL(urlString);
 	const hostPath = `${urlobj.hostname}${urlobj.pathname}`;
@@ -10,4 +40,5 @@ function normalizeURL(urlString) {
 
 module.exports = {
 	normalizeURL,
+	getURLsFromHTML,
 };
--- crawl.test.js
+++ crawl.test.js
@@ -1,28 +1,75 @@
-const { normalizeURL } = require("./crawl.js");
+const { normalizeURL, getURLsFromHTML } = require("./crawl.js");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeURL remove protocol", () => {
-	const input = "https://bento.me/divyanshbaghel";
+	const input = "https://bento.me";
 	const actual = normalizeURL(input);
-	const expected = "bento.me/divyanshbaghel";
+	const expected = "bento.me";
 	expect(actual).toEqual(expected);
 });
 test("normalizeURL remove trailing slash", () => {
-	const input = "https://bento.me/divyanshbaghel/";
+	const input = "https://bento.me/";
 	const actual = normalizeURL(input);
-	const expected = "bento.me/divyanshbaghel";
+	const expected = "bento.me";
 	expect(actual).toEqual(expected);
 });
 test("normalizeURL Capitals", () => {
-	const input = "https://BENTO.me/divyanshbaghel/";
+	const input = "https://BENTO.me";
 	const actual = normalizeURL(input);
-	const expected = "bento.me/divyanshbaghel";
+	const expected = "bento.me";
 	expect(actual).toEqual(expected);
 });
-test("normalizeURL ", () => {
-	const input = "https://BENTO.me/divyanshbaghel/";
-	const actual = normalizeURL(input);
-	const expected = "bento.me/divyanshbaghel";
+test("getURLsFromHTML Absolute", () => {
+	const inputHtmlBody = `
+    <html>
+    <body>
+    <a href = "https://bento.me/divyanshbaghel">bento.me/divyanshbaghel</a>
+    </body>
+    </html>
+    `;
+	const inputBaseURL = "https://bento.me";
+	const actual = getURLsFromHTML(inputHtmlBody, inputBaseURL);
+	const expected = ["https://bento.me/divyanshbaghel"];
+	expect(actual).toEqual(expected);
+});
+test("getURLsFromHTML Relative", () => {
+	const inputHtmlBody = `
+    <html>
+    <body>
+    <a href = "/divyanshbaghel">bento.me/divyanshbaghel</a>
+    </body>
+    </html>
+    `;
+	const inputBaseURL = "https://bento.me";
+	const actual = getURLsFromHTML(inputHtmlBody, inputBaseURL);
+	const expected = ["https://bento.me/divyanshbaghel"];
+	expect(actual).toEqual(expected);
+});
+test("getURLsFromHTML Multiple Links", () => {
+	const inputHtmlBody = `
+    <html>
+    <body>
+    <a href = "https://bento.me/divyanshbaghel">bento.me/divyanshbaghel</a>
+    <a href = "https://bento.me/pragyajoshi">bento.me/pragyajoshi</a>
+    </body>
+    </html>
+    `;
+	const inputBaseURL = "https://bento.me";
+	const actual = getURLsFromHTML(inputHtmlBody, inputBaseURL);
+	const expected = ["https://bento.me/divyanshbaghel","https://bento.me/pragyajoshi"];
+	expect(actual).toEqual(expected);
+});
+test("getURLsFromHTML Invalid", () => {
+	const inputHtmlBody = `
+    <html>
+    <body>
+    <a href = "invalid">INVALIDl</a>
+    </body>
+    </html>
+    `;
+	const inputBaseURL = "https://bento.me";
+	const actual = getURLsFromHTML(inputHtmlBody, inputBaseURL);
+	const expected = [];
 	expect(actual).toEqual(expected);
 });
 
